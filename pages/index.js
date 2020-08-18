8a73d1802b41967d52004dda143665ebd28dd8e5
Otto <zaiser@gmail.com>
2020-08-17 21:16:05 -0300

    Fix compile index

--- pages/index.js
+++ pages/index.js
@@ -39,16 +39,16 @@ export default function Home() {
 				<meta property="og:site_name" content="vertempo" />
 				<meta property="og:type" content="website" />
 				<meta property="og:title" content="vertempo" />
-				<meta property="og:image" content="https://vertempo.netlify.app/rrss.png"></meta>
+				<meta property="og:image" content="https://vertempo.netlify.app/rrss.png" />
 				<meta name="twitter:card" content="summary_large_image" />
-				<meta name="twitter:site" content="@ottocapo" />		
-				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-				<link rel="manifest" href="/site.webmanifest">
-				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-				<meta name="msapplication-TileColor" content="#603cba">
-				<meta name="theme-color" content="#ffffff">
+				<meta name="twitter:site" content="@ottocapo" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+				<link rel="manifest" href="/site.webmanifest" />
+				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+				<meta name="msapplication-TileColor" content="#603cba" />
+				<meta name="theme-color" content="#ffffff" />
 			</Head>
 			<Header />
 			<main className="container">
