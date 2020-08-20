c62b7386ba34f7dfcb86b2e0628786a939c2eb74
Otto <zaiser@gmail.com>
2020-08-20 18:22:16 -0300

    Config again

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -4,22 +4,24 @@ export default function Footer() {
 	return (
 		<footer className="footer">
 			<div className="container">
-				{/* <nav className="nav">
+				<div className="foot">
+					{/* <nav className="nav">
 					<ul className="menu">
 						<li>
-							<a href="https://cafecito.app/otto" target="_blank">
-								Colaborá con nosotros
-							</a>
+						<a href="https://cafecito.app/otto" target="_blank">
+						Colaborá con nosotros
+						</a>
 						</li>
-					</ul>
-				</nav> */}
-				<div className="made">
-					<div>
-						Hecho con <span aria-hidden="true">❤️</span>
-						<span className="sr-only">amor</span> por <a href="https://otto.ar/">Otto</a> & Sole
+						</ul>
+					</nav> */}
+					<div className="made">
+						<div>
+							Hecho con <span aria-hidden="true">❤️</span>
+							<span className="sr-only">amor</span> por <a href="https://otto.ar/">Otto</a> & Sole
+						</div>
 					</div>
+					<Sugerencia />
 				</div>
-				<Sugerencia />
 			</div>
 		</footer>
 	);
